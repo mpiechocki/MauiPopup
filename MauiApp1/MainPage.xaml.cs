5b97ddfd1ddcfa5606f30ec85f797904e7c8db4a
Mikołaj Piechocki <piechocki.mikolaj@gmail.com>
2025-03-03 09:07:20 -0500

    Code

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		var popup = new NewPopup();
+		Shell.Current.ShowPopup(popup);
+	}
+}
+
---
+++ MauiApp1/NewPopup.xaml.cs
@@ -0,0 +1,11 @@
+using CommunityToolkit.Maui.Views;
+
+namespace MauiApp1;
+
+public partial class NewPopup : Popup
+{
+	public NewPopup()
+	{
+		InitializeComponent();
+	}
+}
